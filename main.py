fe7a9cd6b42e2b5882a70d012adc87d58444e286
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2025-03-20 14:39:32 -0500

    Define Character Count Function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count,get_char_count
 
 def get_book_text(filepath):
     # Return text in file as a string 
@@ -7,7 +7,10 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    num_words = get_word_count(book_text)
-    print(f"{num_words} words found in the document")
+    word_count = get_word_count(book_text)
+    char_count = get_char_count(book_text)
+
+    print(f"{word_count} words found in the document")
+    print(char_count)
 
 main() # Run Program
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def get_word_count(string):
     # Return word count as an int
     words = string.split()
     return len(words)
+
+def get_char_count(string):
+    result = dict()
+    for char in string:
+        char = char.lower()
+        if char not in result:
+            result[char] = 0
+        result[char] += 1
+    return result
