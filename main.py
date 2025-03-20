25e7c704b3d059858b40f2dde8b168cdae018665
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2025-03-20 14:23:13 -0500

    Word Count/File Reading

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(filepath):
+    # Return text in file as a string 
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = get_word_count(book_text)
+    print(f"{num_words} words found in the document")
+
+main() # Run Program
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_word_count(string):
+    # Return word count as an int
+    words = string.split()
+    return len(words)
